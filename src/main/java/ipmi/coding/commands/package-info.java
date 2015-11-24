fd12f719e991613bfc37eb2b58d38c593443effc
luolvming <luolvming@users.noreply.github.com>
2015-11-24 12:28:43 +0800

    Update package-info.java

--- src/main/java/ipmi/coding/commands/package-info.java
+++ src/main/java/ipmi/coding/commands/package-info.java
@@ -2,4 +2,5 @@
  * The package containing command wrappers. These include command encoders and decoders. 
  * To add custom IPMI command extend {@link ipmi.coding.commands.IpmiCommandCoder} class and implement {@link ipmi.api.async.messages.IpmiResponseData} interface.
  */
+ 
 package ipmi.coding.commands;
